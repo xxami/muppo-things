bc801f7f8aeab780010d7ada1d7f02e556e675ce
lilah <lilah.willacott@gmail.com>
2017-02-09 20:56:24 +0000

    determine transaction type; very oneof abuse

--- Cocopops/Cocopops/Program.cs
+++ Cocopops/Cocopops/Program.cs
@@ -5,6 +5,7 @@ using Cocopops.Model.BaseTransaction;
 using Cocopops.Model.ExtendedTransaction;
 using Cocopops.Model.Transaction;
 using Cocopops.Model.TransactionCollection;
+using System;
 
 namespace Cocopops
 {
@@ -20,6 +21,7 @@ namespace Cocopops
                     new BaseTransaction {ExecutingEntityCode = "neko1", TransactionReferenceNumber = "donuts0001"}
             };
 
+
             // A second transaction, using extra fields provided by ExtendedTransaction type
             var secondTransaction = new Transaction
             {
@@ -32,23 +34,39 @@ namespace Cocopops
                 }
             };
 
+
             // Two transactions is maaaany transactions
             var allTheTransactions = new TransactionCollection {Transaction = {firstTransaction, secondTransaction}};
 
             // Serialize all of these things to a file
             using (var outputStream = File.Create("SecretTransactions.dat")) allTheTransactions.WriteTo(outputStream);
 
+
             // Deserialize
             TransactionCollection transactionCollection;
             using (var inputStream = File.OpenRead("SecretTransactions.dat"))
                 transactionCollection = TransactionCollection.Parser.ParseFrom(inputStream);
 
+
             // Does it work?
-            foreach (var transaction in transactionCollection.Transaction)
-            {
-                transaction.TransactionCase
+            foreach (var transaction in transactionCollection.Transaction) {
+
+                // Oneof determination
+                switch (transaction.TransactionCase)
+                {
+                    case Transaction.TransactionOneofCase.BaseTransaction:
+                        Console.WriteLine("{0}/{1}", transaction.BaseTransaction.ExecutingEntityCode, transaction.BaseTransaction.TransactionReferenceNumber);
+                        break;
+
+                    case Transaction.TransactionOneofCase.ExtendedTransaction:
+                        Console.WriteLine("{0}/{1}/{2}/{3}", transaction.ExtendedTransaction.BaseTransaction.ExecutingEntityCode,
+                            transaction.ExtendedTransaction.BaseTransaction.TransactionReferenceNumber,
+                            transaction.ExtendedTransaction.Comments, transaction.ExtendedTransaction.Extra);
+                        break;
+                }
             }
 
+            Console.ReadKey();
         }
     }
 }
